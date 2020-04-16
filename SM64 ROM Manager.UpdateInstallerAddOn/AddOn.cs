5a541d5976325a05cd80f22a13ea5dba17f42801
Pilzinsel64 <pilzinsel64@gmx.de>
2020-04-16 13:24:11 +0200

    extend update installer addon

--- SM64 ROM Manager.UpdateInstallerAddOn/AddOn.cs
+++ SM64 ROM Manager.UpdateInstallerAddOn/AddOn.cs
@@ -7,15 +7,11 @@ namespace UpdateInstaller
     {
         public static void Main(UpdateInstallerConfig config, bool isAfterCopyFiles)
         {
-            if (!isAfterCopyFiles && config.NewApplicationVersion is null && config.CurrentApplicationVersion is null)
+            if (!isAfterCopyFiles /*&& config.NewApplicationVersion is null && config.CurrentApplicationVersion is null*/)
             {
-                foreach (string dir in Directory.GetDirectories(Path.Combine(config.HostApplicationPath, @"Data\Tweaks")))
-                {
-                    if (Path.GetFileName(dir) == "Reviewed")
-                        Directory.Delete(dir, true);
-                }
-
                 Directory.Delete(Path.Combine(config.HostApplicationPath, @"Data\Importer Presets"), true);
+                Directory.Delete(Path.Combine(config.HostApplicationPath, @"Data\Custom Objects\Reviewed"), true);
+                Directory.Delete(Path.Combine(config.HostApplicationPath, @"Data\Tweaks\Reviewed"), true);
             }
         }
     }
