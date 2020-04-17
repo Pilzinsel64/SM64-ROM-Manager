ae1b1b4c580baeb1a7bc87d79d235cf1610b838f
Pilzinsel64 <pilzinsel64@gmx.de>
2020-04-17 08:29:58 +0200

    add new patreon donator to about window

--- SM64 ROM Manager/RomManager/GUI/Other/Form_About.Designer.cs
+++ SM64 ROM Manager/RomManager/GUI/Other/Form_About.Designer.cs
@@ -37,98 +37,53 @@ namespace SM64_ROM_Manager
         [DebuggerStepThrough()]
         private void InitializeComponent()
         {
-            var resources = new System.ComponentModel.ComponentResourceManager(typeof(Form_About));
-            _LabelX1 = new DevComponents.DotNetBar.LabelX();
-            _PictureBox_Donate = new PictureBox();
-            _PictureBox_Donate.Click += new EventHandler(PictureBox_Donate_Click);
-            ((System.ComponentModel.ISupportInitialize)_PictureBox_Donate).BeginInit();
-            SuspendLayout();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form_About));
+            this.LabelX1 = new DevComponents.DotNetBar.LabelX();
+            this.PictureBox_Donate = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBox_Donate)).BeginInit();
+            this.SuspendLayout();
             // 
             // LabelX1
             // 
-            resources.ApplyResources(_LabelX1, "LabelX1");
+            resources.ApplyResources(this.LabelX1, "LabelX1");
             // 
             // 
             // 
-            _LabelX1.BackgroundStyle.CornerType = DevComponents.DotNetBar.eCornerType.Square;
-            _LabelX1.Name = "LabelX1";
-            _LabelX1.Style = DevComponents.DotNetBar.eDotNetBarStyle.VS2005;
-            _LabelX1.TextLineAlignment = StringAlignment.Near;
+            this.LabelX1.BackgroundStyle.CornerType = DevComponents.DotNetBar.eCornerType.Square;
+            this.LabelX1.Name = "LabelX1";
+            this.LabelX1.Style = DevComponents.DotNetBar.eDotNetBarStyle.VS2005;
+            this.LabelX1.TextLineAlignment = System.Drawing.StringAlignment.Near;
             // 
             // PictureBox_Donate
             // 
-            resources.ApplyResources(_PictureBox_Donate, "PictureBox_Donate");
-            _PictureBox_Donate.BackColor = Color.Transparent;
-            _PictureBox_Donate.Cursor = Cursors.Hand;
-            _PictureBox_Donate.Image = My.Resources.Resources.btn_donateCC_LG;
-            _PictureBox_Donate.Name = "PictureBox_Donate";
-            _PictureBox_Donate.TabStop = false;
+            resources.ApplyResources(this.PictureBox_Donate, "PictureBox_Donate");
+            this.PictureBox_Donate.BackColor = System.Drawing.Color.Transparent;
+            this.PictureBox_Donate.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.PictureBox_Donate.Image = global::SM64_ROM_Manager.My.Resources.Resources.btn_donateCC_LG;
+            this.PictureBox_Donate.Name = "PictureBox_Donate";
+            this.PictureBox_Donate.TabStop = false;
+            this.PictureBox_Donate.Click += new System.EventHandler(this.PictureBox_Donate_Click);
             // 
             // Form_About
             // 
             resources.ApplyResources(this, "$this");
-            AutoScaleMode = AutoScaleMode.Font;
-            Controls.Add(_PictureBox_Donate);
-            Controls.Add(_LabelX1);
-            FormBorderStyle = FormBorderStyle.FixedToolWindow;
-            MaximizeBox = false;
-            MinimizeBox = false;
-            Name = "Form_About";
-            ShowIcon = false;
-            TopLeftCornerSize = 0;
-            TopRightCornerSize = 0;
-            ((System.ComponentModel.ISupportInitialize)_PictureBox_Donate).EndInit();
-            ResumeLayout(false);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.PictureBox_Donate);
+            this.Controls.Add(this.LabelX1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form_About";
+            this.ShowIcon = false;
+            this.TopLeftCornerSize = 0;
+            this.TopRightCornerSize = 0;
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBox_Donate)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
-        private DevComponents.DotNetBar.LabelX _LabelX1;
+        private DevComponents.DotNetBar.LabelX LabelX1;
 
-        internal DevComponents.DotNetBar.LabelX LabelX1
-        {
-            [MethodImpl(MethodImplOptions.Synchronized)]
-            get
-            {
-                return _LabelX1;
-            }
-
-            [MethodImpl(MethodImplOptions.Synchronized)]
-            set
-            {
-                if (_LabelX1 != null)
-                {
-                }
-
-                _LabelX1 = value;
-                if (_LabelX1 != null)
-                {
-                }
-            }
-        }
-
-        private PictureBox _PictureBox_Donate;
-
-        internal PictureBox PictureBox_Donate
-        {
-            [MethodImpl(MethodImplOptions.Synchronized)]
-            get
-            {
-                return _PictureBox_Donate;
-            }
-
-            [MethodImpl(MethodImplOptions.Synchronized)]
-            set
-            {
-                if (_PictureBox_Donate != null)
-                {
-                    _PictureBox_Donate.Click -= PictureBox_Donate_Click;
-                }
-
-                _PictureBox_Donate = value;
-                if (_PictureBox_Donate != null)
-                {
-                    _PictureBox_Donate.Click += PictureBox_Donate_Click;
-                }
-            }
-        }
+        private PictureBox PictureBox_Donate;
     }
 }
