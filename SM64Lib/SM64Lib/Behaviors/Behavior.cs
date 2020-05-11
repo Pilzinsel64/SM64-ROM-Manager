f264b9a98bb8104d76255c576165ad51216141b2
Pilzinsel64 <pilzinsel64@gmx.de>
2020-05-11 19:31:15 +0200

    copy behavior props

--- SM64Lib/SM64Lib/Behaviors/Behavior.cs
+++ SM64Lib/SM64Lib/Behaviors/Behavior.cs
@@ -8,6 +8,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Z.Collections.Extensions;
+using static SM64Lib.Extensions.ObjectExtensions;
 
 namespace SM64Lib.Behaviors
 {
@@ -226,7 +227,15 @@ namespace SM64Lib.Behaviors
 
         public void CopyPropertiesTo(Behavior dest)
         {
+            // Copy Script
+            TakeoverSettingsToScript();
+            dest.Script.Close();
+            dest.Script.AddRange(Script);
+            dest.ParseScript();
 
+            // Copy Configs
+            dest.Config.Name = Config.Name;
+            Config.ParamsInfo.CloneTo(dest.Config.ParamsInfo);
         }
 
     }
--- SM64Lib/SM64Lib/ExtensionMethods.cs
+++ SM64Lib/SM64Lib/ExtensionMethods.cs
@@ -1,4 +1,7 @@
 ﻿
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
 namespace SM64Lib.Extensions
 {
     public static class Int16Extensions
@@ -6,6 +9,25 @@ namespace SM64Lib.Extensions
         public static bool IsInRange(this short value, short min, short max)
         {
             return value >= min && value <= max;
+        }
+    }
+
+    public static class ObjectExtensions
+    {
+        public static void CloneTo<T>(this T source, T destination) where T : class
+        {
+            var json = JsonSerializer.CreateDefault();
+            var jobj = JObject.FromObject(source);
+            jobj.Populate(destination);
+        }
+    }
+
+    public static class JsonExtensions
+    {
+        public static void Populate(this JToken value, object target)
+        {
+            using (var sr = value.CreateReader())
+                JsonSerializer.CreateDefault().Populate(sr, target);
         }
     }
 }
