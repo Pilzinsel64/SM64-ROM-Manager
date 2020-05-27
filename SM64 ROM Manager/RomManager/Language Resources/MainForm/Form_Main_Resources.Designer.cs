e26fb8e7f6f82900da8b8d3ecbe1e6dd1ea457d0
Pilzinsel64 <pilzinsel64@gmx.de>
2020-05-27 15:15:49 +0200

    Tell user how importing ROM.config is

--- SM64 ROM Manager/RomManager/Language Resources/MainForm/Form_Main_Resources.Designer.cs
+++ SM64 ROM Manager/RomManager/Language Resources/MainForm/Form_Main_Resources.Designer.cs
@@ -287,7 +287,7 @@ namespace SM64_ROM_Manager.My.Resources {
         }
         
         /// <summary>
-        ///   Sucht eine lokalisierte Zeichenfolge, die The ROM will be prepared for use with SM64 ROM Manager. ähnelt.
+        ///   Sucht eine lokalisierte Zeichenfolge, die The ROM will be prepared for use with SM64 ROM Manager.&lt;br/&gt;&lt;br/&gt;Please Note:&lt;br/&gt;While prepairing your ROM, an additional file will be created. It will be named like your ROM but with a &quot;.config&quot; extension.&lt;br/&gt;This file contains many important settings and other stuff to provide some features in the SM64 ROM Manager.&lt;br/&gt;If you delete this .config file, you might get &lt;u&gt;some errors or unwanted behaviors&lt;/u&gt; in ROM Manager.&lt;br/&gt;So, if &lt;u&gt;you rename your ROM file&lt;/u&gt;, &lt;u&gt;always rename the config file&lt;/u&gt; to [Rest der Zeichenfolge wurde abgeschnitten]&quot;; ähnelt.
         /// </summary>
         public static string MsgBox_PrepaireRom {
             get {
