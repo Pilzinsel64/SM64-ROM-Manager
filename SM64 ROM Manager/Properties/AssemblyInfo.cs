7da7928e688c87188a9ff94f5114960d9c4bac61
Pilzinsel64 <pilzinsel64@gmx.de>
2020-06-14 10:14:25 +0200

    change version to v1.9.9

--- SM64 ROM Manager/Properties/AssemblyInfo.cs
+++ SM64 ROM Manager/Properties/AssemblyInfo.cs
@@ -30,5 +30,5 @@ using global::System.Runtime.InteropServices;
 // übernehmen, indem Sie "*" eingeben:
 // <Assembly: AssemblyVersion("1.0.*")> 
 
-[assembly: AssemblyVersion("1.9.8.0")]
+[assembly: AssemblyVersion("1.9.9.0")]
 
