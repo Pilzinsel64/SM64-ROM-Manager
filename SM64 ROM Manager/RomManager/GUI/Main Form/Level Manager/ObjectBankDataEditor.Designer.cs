f0ecc3e253fae5dfb74781266b9b786315d576f4
Pilzinsel64 <pilzinsel64@gmx.de>
2020-07-09 12:43:01 +0200

    fix addition text on german computers

--- SM64 ROM Manager/RomManager/GUI/Main Form/Level Manager/ObjectBankDataEditor.Designer.cs
+++ SM64 ROM Manager/RomManager/GUI/Main Form/Level Manager/ObjectBankDataEditor.Designer.cs
@@ -65,6 +65,7 @@ namespace SM64_ROM_Manager
             // 
             // ObdTree
             // 
+            resources.ApplyResources(this.ObdTree, "ObdTree");
             this.ObdTree.AccessibleRole = System.Windows.Forms.AccessibleRole.Outline;
             this.ObdTree.BackColor = System.Drawing.SystemColors.Window;
             // 
@@ -73,7 +74,6 @@ namespace SM64_ROM_Manager
             this.ObdTree.BackgroundStyle.Class = "TreeBorderKey";
             this.ObdTree.BackgroundStyle.CornerType = DevComponents.DotNetBar.eCornerType.Square;
             this.ObdTree.CellEdit = true;
-            resources.ApplyResources(this.ObdTree, "ObdTree");
             this.ObdTree.DragDropEnabled = false;
             this.ObdTree.DragDropNodeCopyEnabled = false;
             this.ObdTree.LicenseKey = "F962CEC7-CD8F-4911-A9E9-CAB39962FC1F";
@@ -106,8 +106,8 @@ namespace SM64_ROM_Manager
             // 
             // ContextMenuBar1
             // 
-            this.ContextMenuBar1.AntiAlias = true;
             resources.ApplyResources(this.ContextMenuBar1, "ContextMenuBar1");
+            this.ContextMenuBar1.AntiAlias = true;
             this.ContextMenuBar1.IsMaximized = false;
             this.ContextMenuBar1.Items.AddRange(new DevComponents.DotNetBar.BaseItem[] {
             this.ButtonItem_CM_ObdList,
@@ -123,108 +123,108 @@ namespace SM64_ROM_Manager
             // 
             // ButtonItem_CM_ObdList
             // 
+            resources.ApplyResources(this.ButtonItem_CM_ObdList, "ButtonItem_CM_ObdList");
             this.ButtonItem_CM_ObdList.AutoExpandOnClick = true;
             this.ButtonItem_CM_ObdList.Name = "ButtonItem_CM_ObdList";
             this.ButtonItem_CM_ObdList.SubItems.AddRange(new DevComponents.DotNetBar.BaseItem[] {
             this.ButtonItem_AddObd,
             this.ButtonItem_RemoveObdList});
-            resources.ApplyResources(this.ButtonItem_CM_ObdList, "ButtonItem_CM_ObdList");
             // 
             // ButtonItem_AddObd
             // 
+            resources.ApplyResources(this.ButtonItem_AddObd, "ButtonItem_AddObd");
             this.ButtonItem_AddObd.Image = global::SM64_ROM_Manager.My.Resources.MyIcons.icons8_plus_math_16px;
             this.ButtonItem_AddObd.Name = "ButtonItem_AddObd";
-            resources.ApplyResources(this.ButtonItem_AddObd, "ButtonItem_AddObd");
             this.ButtonItem_AddObd.Click += new System.EventHandler(this.ButtonItem_AddObd_Click);
             // 
             // ButtonItem_RemoveObdList
             // 
+            resources.ApplyResources(this.ButtonItem_RemoveObdList, "ButtonItem_RemoveObdList");
             this.ButtonItem_RemoveObdList.BeginGroup = true;
             this.ButtonItem_RemoveObdList.Image = global::SM64_ROM_Manager.My.Resources.MyIcons.icons8_delete_sign_16px;
             this.ButtonItem_RemoveObdList.Name = "ButtonItem_RemoveObdList";
-            resources.ApplyResources(this.ButtonItem_RemoveObdList, "ButtonItem_RemoveObdList");
             this.ButtonItem_RemoveObdList.Click += new System.EventHandler(this.ButtonItem_RemoveObdList_Click);
             // 
             // ButtonItem_CM_Obd
             // 
+            resources.ApplyResources(this.ButtonItem_CM_Obd, "ButtonItem_CM_Obd");
             this.ButtonItem_CM_Obd.AutoExpandOnClick = true;
             this.ButtonItem_CM_Obd.Name = "ButtonItem_CM_Obd";
             this.ButtonItem_CM_Obd.SubItems.AddRange(new DevComponents.DotNetBar.BaseItem[] {
             this.ButtonItem_RemoveObd});
-            resources.ApplyResources(this.ButtonItem_CM_Obd, "ButtonItem_CM_Obd");
             // 
             // ButtonItem_RemoveObd
             // 
+            resources.ApplyResources(this.ButtonItem_RemoveObd, "ButtonItem_RemoveObd");
             this.ButtonItem_RemoveObd.BeginGroup = true;
             this.ButtonItem_RemoveObd.Image = global::SM64_ROM_Manager.My.Resources.MyIcons.icons8_delete_sign_16px;
             this.ButtonItem_RemoveObd.Name = "ButtonItem_RemoveObd";
-            resources.ApplyResources(this.ButtonItem_RemoveObd, "ButtonItem_RemoveObd");
             this.ButtonItem_RemoveObd.Click += new System.EventHandler(this.ButtonItem_RemoveObd_Click);
             // 
             // ButtonItem_CM_Cmds
             // 
+            resources.ApplyResources(this.ButtonItem_CM_Cmds, "ButtonItem_CM_Cmds");
             this.ButtonItem_CM_Cmds.AutoExpandOnClick = true;
             this.ButtonItem_CM_Cmds.Name = "ButtonItem_CM_Cmds";
             this.ButtonItem_CM_Cmds.SubItems.AddRange(new DevComponents.DotNetBar.BaseItem[] {
             this.ButtonItem_AddCmd});
-            resources.ApplyResources(this.ButtonItem_CM_Cmds, "ButtonItem_CM_Cmds");
             // 
             // ButtonItem_AddCmd
             // 
+            resources.ApplyResources(this.ButtonItem_AddCmd, "ButtonItem_AddCmd");
             this.ButtonItem_AddCmd.Image = global::SM64_ROM_Manager.My.Resources.MyIcons.icons8_plus_math_16px;
             this.ButtonItem_AddCmd.Name = "ButtonItem_AddCmd";
-            resources.ApplyResources(this.ButtonItem_AddCmd, "ButtonItem_AddCmd");
             this.ButtonItem_AddCmd.Click += new System.EventHandler(this.ButtonItem_AddCmd_Click);
             // 
             // ButtonItem_CM_Cmd
             // 
+            resources.ApplyResources(this.ButtonItem_CM_Cmd, "ButtonItem_CM_Cmd");
             this.ButtonItem_CM_Cmd.AutoExpandOnClick = true;
             this.ButtonItem_CM_Cmd.Name = "ButtonItem_CM_Cmd";
             this.ButtonItem_CM_Cmd.SubItems.AddRange(new DevComponents.DotNetBar.BaseItem[] {
             this.ButtonItem_RemoveCmd});
-            resources.ApplyResources(this.ButtonItem_CM_Cmd, "ButtonItem_CM_Cmd");
             // 
             // ButtonItem_RemoveCmd
             // 
+            resources.ApplyResources(this.ButtonItem_RemoveCmd, "ButtonItem_RemoveCmd");
             this.ButtonItem_RemoveCmd.Image = global::SM64_ROM_Manager.My.Resources.MyIcons.icons8_delete_sign_16px;
             this.ButtonItem_RemoveCmd.Name = "ButtonItem_RemoveCmd";
-            resources.ApplyResources(this.ButtonItem_RemoveCmd, "ButtonItem_RemoveCmd");
             this.ButtonItem_RemoveCmd.Click += new System.EventHandler(this.ButtonItem_RemoveCmd_Click);
             // 
             // ButtonItem_CM_Objs
             // 
+            resources.ApplyResources(this.ButtonItem_CM_Objs, "ButtonItem_CM_Objs");
             this.ButtonItem_CM_Objs.AutoExpandOnClick = true;
             this.ButtonItem_CM_Objs.Name = "ButtonItem_CM_Objs";
             this.ButtonItem_CM_Objs.SubItems.AddRange(new DevComponents.DotNetBar.BaseItem[] {
             this.ButtonItem_AddObj});
-            resources.ApplyResources(this.ButtonItem_CM_Objs, "ButtonItem_CM_Objs");
             // 
             // ButtonItem_AddObj
             // 
+            resources.ApplyResources(this.ButtonItem_AddObj, "ButtonItem_AddObj");
             this.ButtonItem_AddObj.Image = global::SM64_ROM_Manager.My.Resources.MyIcons.icons8_plus_math_16px;
             this.ButtonItem_AddObj.Name = "ButtonItem_AddObj";
-            resources.ApplyResources(this.ButtonItem_AddObj, "ButtonItem_AddObj");
             this.ButtonItem_AddObj.Click += new System.EventHandler(this.ButtonItem_AddObj_Click);
             // 
             // ButtonItem_CM_Obj
             // 
+            resources.ApplyResources(this.ButtonItem_CM_Obj, "ButtonItem_CM_Obj");
             this.ButtonItem_CM_Obj.AutoExpandOnClick = true;
             this.ButtonItem_CM_Obj.Name = "ButtonItem_CM_Obj";
             this.ButtonItem_CM_Obj.SubItems.AddRange(new DevComponents.DotNetBar.BaseItem[] {
             this.ButtonItem_RemoveObj});
-            resources.ApplyResources(this.ButtonItem_CM_Obj, "ButtonItem_CM_Obj");
             // 
             // ButtonItem_RemoveObj
             // 
+            resources.ApplyResources(this.ButtonItem_RemoveObj, "ButtonItem_RemoveObj");
             this.ButtonItem_RemoveObj.Image = global::SM64_ROM_Manager.My.Resources.MyIcons.icons8_delete_sign_16px;
             this.ButtonItem_RemoveObj.Name = "ButtonItem_RemoveObj";
-            resources.ApplyResources(this.ButtonItem_RemoveObj, "ButtonItem_RemoveObj");
             this.ButtonItem_RemoveObj.Click += new System.EventHandler(this.ButtonItem_RemoveObj_Click);
             // 
             // buttonX1
             // 
-            this.buttonX1.AccessibleRole = System.Windows.Forms.AccessibleRole.PushButton;
             resources.ApplyResources(this.buttonX1, "buttonX1");
+            this.buttonX1.AccessibleRole = System.Windows.Forms.AccessibleRole.PushButton;
             this.buttonX1.AutoExpandOnClick = true;
             this.buttonX1.ColorTable = DevComponents.DotNetBar.eButtonColor.OrangeWithBackground;
             this.buttonX1.Image = global::SM64_ROM_Manager.My.Resources.MyIcons.icons8_expand_arrow_16px;
@@ -237,16 +237,16 @@ namespace SM64_ROM_Manager
             // 
             // ButtonItem_Export
             // 
+            resources.ApplyResources(this.ButtonItem_Export, "ButtonItem_Export");
             this.ButtonItem_Export.GlobalItem = false;
             this.ButtonItem_Export.Name = "ButtonItem_Export";
-            resources.ApplyResources(this.ButtonItem_Export, "ButtonItem_Export");
             this.ButtonItem_Export.Click += new System.EventHandler(this.ButtonItem_Export_Click);
             // 
             // ButtonItem_Import
             // 
+            resources.ApplyResources(this.ButtonItem_Import, "ButtonItem_Import");
             this.ButtonItem_Import.GlobalItem = false;
             this.ButtonItem_Import.Name = "ButtonItem_Import";
-            resources.ApplyResources(this.ButtonItem_Import, "ButtonItem_Import");
             this.ButtonItem_Import.Click += new System.EventHandler(this.ButtonItem_Import_Click);
             // 
             // ObjectBankDataEditor
