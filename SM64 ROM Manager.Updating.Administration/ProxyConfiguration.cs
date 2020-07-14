a5662282d149017b8fd6260c344130b1026d373b
Pilzinsel64 <pilzinsel64@gmx.de>
2020-07-14 15:43:04 +0200

    add proxy configuration to update project

---
+++ SM64 ROM Manager.Updating.Administration/ProxyConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using Pilz.Cryptography;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SM64_ROM_Manager.Updating.Administration
+{
+    public class ProxyConfiguration
+    {
+        public string Username { get; set; }
+        public SecureString Password { get; set; }
+    }
+}
