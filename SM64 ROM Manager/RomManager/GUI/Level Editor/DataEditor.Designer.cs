2d83f246a655ff0ac55f9be0b407ff5d06bf57e3
Pilzinsel64 <pilzinsel64@gmx.de>
2020-07-28 14:09:00 +0200

    base GUI layout for Data Editor

--- SM64 ROM Manager/RomManager/GUI/Level Editor/DataEditor.Designer.cs
+++ SM64 ROM Manager/RomManager/GUI/Level Editor/DataEditor.Designer.cs
@@ -29,23 +29,121 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(DataEditor));
+            this.superGridControl1 = new DevComponents.DotNetBar.SuperGrid.SuperGridControl();
+            this.bar1 = new DevComponents.DotNetBar.Bar();
+            this.buttonItem_File = new DevComponents.DotNetBar.ButtonItem();
+            this.buttonItem2 = new DevComponents.DotNetBar.ButtonItem();
+            this.CheckBoxItem_Objects = new DevComponents.DotNetBar.CheckBoxItem();
+            this.CheckBoxItem_Warps = new DevComponents.DotNetBar.CheckBoxItem();
+            this.buttonItem1 = new DevComponents.DotNetBar.ButtonItem();
+            this.buttonItem3 = new DevComponents.DotNetBar.ButtonItem();
+            ((System.ComponentModel.ISupportInitialize)(this.bar1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // superGridControl1
+            // 
+            this.superGridControl1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.superGridControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.superGridControl1.FilterExprColors.SysFunction = System.Drawing.Color.DarkRed;
+            this.superGridControl1.ForeColor = System.Drawing.Color.Black;
+            this.superGridControl1.LicenseKey = "F962CEC7-CD8F-4911-A9E9-CAB39962FC1F";
+            this.superGridControl1.Location = new System.Drawing.Point(0, 24);
+            this.superGridControl1.Name = "superGridControl1";
+            this.superGridControl1.Size = new System.Drawing.Size(800, 426);
+            this.superGridControl1.TabIndex = 0;
+            this.superGridControl1.Text = "superGridControl1";
+            // 
+            // bar1
+            // 
+            this.bar1.AccessibleDescription = "bar1 (bar1)";
+            this.bar1.AccessibleName = "bar1";
+            this.bar1.AccessibleRole = System.Windows.Forms.AccessibleRole.MenuBar;
+            this.bar1.AntiAlias = true;
+            this.bar1.BarType = DevComponents.DotNetBar.eBarType.MenuBar;
+            this.bar1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.bar1.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.bar1.IsMaximized = false;
+            this.bar1.Items.AddRange(new DevComponents.DotNetBar.BaseItem[] {
+            this.buttonItem_File,
+            this.buttonItem2});
+            this.bar1.Location = new System.Drawing.Point(0, 0);
+            this.bar1.MenuBar = true;
+            this.bar1.Name = "bar1";
+            this.bar1.Size = new System.Drawing.Size(800, 24);
+            this.bar1.Stretch = true;
+            this.bar1.Style = DevComponents.DotNetBar.eDotNetBarStyle.StyleManagerControlled;
+            this.bar1.TabIndex = 2;
+            this.bar1.TabStop = false;
+            this.bar1.Text = "bar1";
+            // 
+            // buttonItem_File
+            // 
+            this.buttonItem_File.Name = "buttonItem_File";
+            this.buttonItem_File.SubItems.AddRange(new DevComponents.DotNetBar.BaseItem[] {
+            this.buttonItem1,
+            this.buttonItem3});
+            this.buttonItem_File.Text = "File";
+            // 
+            // buttonItem2
+            // 
+            this.buttonItem2.Name = "buttonItem2";
+            this.buttonItem2.SubItems.AddRange(new DevComponents.DotNetBar.BaseItem[] {
+            this.CheckBoxItem_Objects,
+            this.CheckBoxItem_Warps});
+            this.buttonItem2.Text = "View";
+            // 
+            // CheckBoxItem_Objects
+            // 
+            this.CheckBoxItem_Objects.CheckBoxStyle = DevComponents.DotNetBar.eCheckBoxStyle.RadioButton;
+            this.CheckBoxItem_Objects.Name = "CheckBoxItem_Objects";
+            this.CheckBoxItem_Objects.Text = "Objects";
+            // 
+            // CheckBoxItem_Warps
+            // 
+            this.CheckBoxItem_Warps.CheckBoxStyle = DevComponents.DotNetBar.eCheckBoxStyle.RadioButton;
+            this.CheckBoxItem_Warps.Name = "CheckBoxItem_Warps";
+            this.CheckBoxItem_Warps.Text = "Warps";
+            // 
+            // buttonItem1
+            // 
+            this.buttonItem1.Image = global::SM64_ROM_Manager.My.Resources.MyIcons.icons8_import_16px;
+            this.buttonItem1.Name = "buttonItem1";
+            this.buttonItem1.Text = "Import ...";
+            // 
+            // buttonItem3
+            // 
+            this.buttonItem3.Image = global::SM64_ROM_Manager.My.Resources.MyIcons.icons8_export_16px;
+            this.buttonItem3.Name = "buttonItem3";
+            this.buttonItem3.Text = "Export ...";
             // 
             // DataEditor
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.superGridControl1);
+            this.Controls.Add(this.bar1);
+            this.ForeColor = System.Drawing.Color.Black;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "DataEditor";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Data Editor";
             this.TopLeftCornerSize = 0;
             this.TopRightCornerSize = 0;
+            ((System.ComponentModel.ISupportInitialize)(this.bar1)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private DevComponents.DotNetBar.SuperGrid.SuperGridControl superGridControl1;
+        private DevComponents.DotNetBar.Bar bar1;
+        private DevComponents.DotNetBar.ButtonItem buttonItem_File;
+        private DevComponents.DotNetBar.ButtonItem buttonItem2;
+        private DevComponents.DotNetBar.CheckBoxItem CheckBoxItem_Objects;
+        private DevComponents.DotNetBar.CheckBoxItem CheckBoxItem_Warps;
+        private DevComponents.DotNetBar.ButtonItem buttonItem1;
+        private DevComponents.DotNetBar.ButtonItem buttonItem3;
     }
 }
