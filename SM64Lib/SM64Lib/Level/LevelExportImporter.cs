b5cd8d81d27806fb5e64918a7efb7a37053fcdf9
Pilzinsel64 <pilzinsel64@gmx.de>
2020-07-28 08:02:10 +0200

    implement import function for leevl export feature

--- SM64Lib/SM64Lib/Level/LevelExportImporter.cs
+++ SM64Lib/SM64Lib/Level/LevelExportImporter.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SM64Lib.Levels.Script;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,52 +9,61 @@ namespace SM64Lib.Levels
 {
     public static class LevelExportImporter
     {
-        private static void CheckParameters(LevelExport export, object destObject, LevelExportContentType requiedContentType, LevelExportImportParameters parameters)
+        public static void ImportLevel(LevelExport export, RomManager destRomManager, KeyValuePair<Level, short>[] levelIDs)
         {
-            if (requiedContentType != LevelExportContentType.Unknown && export.ContentType != requiedContentType)
-                throw new NotSupportedException("Wrong content type!");
-
-            if (destObject == null)
-                throw new ArgumentNullException("The given destination object was null.");
-
-            if (parameters == null)
-                throw new ArgumentNullException("Parameters was null");
-        }
-
-        public static void ImportLevel(LevelExport export, RomManager destRomManager, LevelExportImportParameters parameters)
-        {
-            CheckParameters(export, destRomManager, LevelExportContentType.Level, parameters);
-
             var levels = export.Content as Level[];
             if (levels != null)
             {
                 foreach (var level in levels)
                 {
-                    throw new NotImplementedException();
-                    destRomManager.Levels.Add(level);
+                    var id = (ushort?)levelIDs.FirstOrDefault(n => n.Key == level).Value;
+                    if (id is object)
+                    {
+                        level.LevelID = (ushort)id;
+                        destRomManager.Levels.Add(level);
+                    }
                 }
             }
         }
 
-        public static void ImportArea(LevelExport export, Level destLevel, LevelExportImportParameters parameters)
+        public static void ImportArea(LevelExport export, Level destLevel, KeyValuePair<LevelArea, byte>[] areaIDs)
         {
-            CheckParameters(export, destLevel, LevelExportContentType.Area, parameters);
-
             var areas = export.Content as LevelArea[];
             if (areas != null)
             {
                 foreach (var area in areas)
                 {
-                    throw new NotImplementedException();
-                    destLevel.Areas.Add(area);
+                    var id = (byte?)areaIDs.FirstOrDefault(n => n.Key == area).Value;
+                    if (id is object)
+                    {
+                        area.AreaID = (byte)id;
+                        destLevel.Areas.Add(area);
+                    }
                 }
             }
         }
 
-        public static void ImportScript(LevelExport export, LevelArea destArea, LevelExportImportParameters parameters)
+        public static void ImportScript(LevelExport export, LevelArea destArea, LevelscriptCommand[] allowedCommands)
         {
-            CheckParameters(export, destArea, LevelExportContentType.Unknown, parameters);
-            throw new NotImplementedException();
+            var cmds = export.Content as LevelscriptCommand[];
+            if (cmds is object)
+            {
+                foreach (var cmd in cmds)
+                {
+                    if (allowedCommands.Contains(cmd))
+                    {
+                        switch (export.ContentType)
+                        {
+                            case LevelExportContentType.Objects:
+                                destArea.Objects.Add(cmd);
+                                break;
+                            case LevelExportContentType.Warps:
+                                destArea.Warps.Add(cmd);
+                                break;
+                        }
+                    }
+                }
+            }
         }
     }
 }
