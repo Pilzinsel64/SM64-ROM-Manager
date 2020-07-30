292dfc78cf2789b376bfcf48c76bf5e860ac76c1
Pilzinsel64 <pilzinsel64@gmx.de>
2020-07-30 17:27:26 +0200

    begin with RomConfig-Editor plugin

---
+++ SM64 ROM Manager.Plugins.RomConfigEditor/Plugin.cs
@@ -0,0 +1,22 @@
+﻿using Pilz.Reflection.PluginSystem.Attributes;
+using SM64Lib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public static class Plugin
+{
+    [LoadMethod()]
+    public static void Main()
+    {
+
+    }
+
+    [PluginFunction("pluginmenur")]
+    public static void OpenEditor(RomManager romManager)
+    {
+
+    }
+}
