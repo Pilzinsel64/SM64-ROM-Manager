c17059e2b705ac5f3995e3624caec8e519c97bc9
Pilzinsel64 <pilzinsel64@gmx.de>
2020-08-17 19:45:37 +0200

    fix broken line breaks on importing text tables

--- SM64Lib.Text.Exporters/ExcelExporter.cs
+++ SM64Lib.Text.Exporters/ExcelExporter.cs
@@ -86,7 +86,7 @@ namespace SM64Lib.Text.Exporters
                             var ri = iti + 2;
                             var c = ws.Cells[ri, 2];
 
-                            ti.Text = ((string)c.Value).Replace("\n", "\r\n");
+                            ti.Text = ((string)c.Value).Replace("\r\n", "\n").Replace("\n", "\r\n");
                         }
                     }
                 }
