6731d68d49fc2e1760bb1bcb3ea9e3e1d532c463
Pilzinsel64 <pilzinsel64@gmx.de>
2020-11-09 11:14:07 +0100

    update version to v1.11 stable

--- SM64 ROM Manager/Properties/Resources.Designer.cs
+++ SM64 ROM Manager/Properties/Resources.Designer.cs
@@ -71,7 +71,7 @@ namespace SM64_ROM_Manager.My.Resources {
         }
         
         /// <summary>
-        ///   Sucht eine lokalisierte Zeichenfolge, die 2 ähnelt.
+        ///   Sucht eine lokalisierte Zeichenfolge, die 1 ähnelt.
         /// </summary>
         internal static string DevelopmentBuild {
             get {
@@ -80,7 +80,7 @@ namespace SM64_ROM_Manager.My.Resources {
         }
         
         /// <summary>
-        ///   Sucht eine lokalisierte Zeichenfolge, die 1 ähnelt.
+        ///   Sucht eine lokalisierte Zeichenfolge, die 0 ähnelt.
         /// </summary>
         internal static string DevelopmentStage {
             get {
