6b6cb624f19fd7cd20482fa6ab54fd2767771754
Pilzinsel64 <pilzinsel64@gmx.de>
2021-01-31 10:57:13 +0100

    change version to v1.12 beta 1

--- SM64 ROM Manager/Properties/Resources.Designer.cs
+++ SM64 ROM Manager/Properties/Resources.Designer.cs
@@ -19,7 +19,7 @@ namespace SM64_ROM_Manager.My.Resources {
     // -Klasse über ein Tool wie ResGen oder Visual Studio automatisch generiert.
     // Um einen Member hinzuzufügen oder zu entfernen, bearbeiten Sie die .ResX-Datei und führen dann ResGen
     // mit der /str-Option erneut aus, oder Sie erstellen Ihr VS-Projekt neu.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "15.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class Resources {
@@ -80,7 +80,7 @@ namespace SM64_ROM_Manager.My.Resources {
         }
         
         /// <summary>
-        ///   Sucht eine lokalisierte Zeichenfolge, die 0 ähnelt.
+        ///   Sucht eine lokalisierte Zeichenfolge, die 2 ähnelt.
         /// </summary>
         internal static string DevelopmentStage {
             get {
