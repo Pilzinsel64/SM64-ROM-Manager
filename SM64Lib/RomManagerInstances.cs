4f82178476689293fd8d01bcd44a2d25ea6ec9d9
Pilzinsel64 <pilzinsel64@gmx.de>
2021-02-09 12:02:07 +0100

    remember all open RomManager Instances (part 2)

---
+++ SM64Lib/RomManagerInstances.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SM64Lib
+{
+    public static class RomManagerInstances
+    {
+        private static readonly List<RomManager> romManagers = new List<RomManager>();
+
+        public static IEnumerable<RomManager> CurrentInstances
+        {
+            get => romManagers;
+        }
+
+        internal static void RegisterRomManager(RomManager romManager)
+        {
+            if(!romManagers.Contains(romManager))
+                romManagers.Add(romManager);
+        }
+
+        internal static void UnregisterRomManager(RomManager romManager)
+        {
+            if (romManagers.Contains(romManager))
+                romManagers.Remove(romManager);
+        }
+    }
+}
