c9e31ac2b19b995eec657f555ad150222a9791ac
Pilzinsel64 <pilzinsel64@gmx.de>
2021-02-20 15:31:11 +0100

    change version to v1.12 Beta 4

--- SM64 ROM Manager/Properties/Resources.Designer.cs
+++ SM64 ROM Manager/Properties/Resources.Designer.cs
@@ -71,7 +71,7 @@ namespace SM64_ROM_Manager.My.Resources {
         }
         
         /// <summary>
-        ///   Sucht eine lokalisierte Zeichenfolge, die 3 ähnelt.
+        ///   Sucht eine lokalisierte Zeichenfolge, die 4 ähnelt.
         /// </summary>
         internal static string DevelopmentBuild {
             get {
