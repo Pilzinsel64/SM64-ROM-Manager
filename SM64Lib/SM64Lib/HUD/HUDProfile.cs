33647b4b6e61d11e4a511244161a25fd7b9fa9ac
Pilzinsel64 <pilzinsel64@gmx.de>
2021-03-11 10:56:49 +0100

    write and save methods for HUDProfile

--- SM64Lib/SM64Lib/HUD/HUDProfile.cs
+++ SM64Lib/SM64Lib/HUD/HUDProfile.cs
@@ -1,6 +1,8 @@
 ﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,13 +11,23 @@ namespace SM64Lib.HUD
 {
     public class HUDProfile
     {
-        public HUDGroup RootGroup { get; set; } = new HUDGroup();
+        public HUDGroup RootGroup { get; } = new HUDGroup();
 
         [JsonIgnore]
         public string ProfileName
         {
             get => RootGroup.Name;
             set => RootGroup.Name = value;
+        }
+
+        public void SaveAs(string filePath)
+        {
+            File.WriteAllText(filePath, JObject.FromObject(this).ToString());
+        }
+
+        public static HUDProfile LoadFrom(string filePath)
+        {
+            return JObject.Parse(File.ReadAllText(filePath)).ToObject<HUDProfile>();
         }
     }
 }
