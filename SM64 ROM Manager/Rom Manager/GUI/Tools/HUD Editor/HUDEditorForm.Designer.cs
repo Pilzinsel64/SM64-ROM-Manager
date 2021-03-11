18638a976cdd97e08612ee7669fc7f700b5098a5
Pilzinsel64 <pilzinsel64@gmx.de>
2021-03-11 10:57:11 +0100

    HUD Editor GUI (beginning)

---
+++ SM64 ROM Manager/Rom Manager/GUI/Tools/HUD Editor/HUDEditorForm.Designer.cs
@@ -0,0 +1,305 @@
+﻿
+namespace SM64_ROM_Manager
+{
+    partial class HUDEditorForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(HUDEditorForm));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.bar1 = new DevComponents.DotNetBar.Bar();
+            this.buttonItem1 = new DevComponents.DotNetBar.ButtonItem();
+            this.buttonItem2 = new DevComponents.DotNetBar.ButtonItem();
+            this.buttonItem3 = new DevComponents.DotNetBar.ButtonItem();
+            this.buttonItem4 = new DevComponents.DotNetBar.ButtonItem();
+            this.buttonItem5 = new DevComponents.DotNetBar.ButtonItem();
+            this.buttonItem6 = new DevComponents.DotNetBar.ButtonItem();
+            this.buttonItem7 = new DevComponents.DotNetBar.ButtonItem();
+            this.buttonX1 = new DevComponents.DotNetBar.ButtonX();
+            this.buttonItem8 = new DevComponents.DotNetBar.ButtonItem();
+            this.buttonItem9 = new DevComponents.DotNetBar.ButtonItem();
+            this.checkBoxX1 = new DevComponents.DotNetBar.Controls.CheckBoxX();
+            this.integerInput1 = new DevComponents.Editors.IntegerInput();
+            this.integerInput2 = new DevComponents.Editors.IntegerInput();
+            this.labelX1 = new DevComponents.DotNetBar.LabelX();
+            this.labelX2 = new DevComponents.DotNetBar.LabelX();
+            this.buttonItem10 = new DevComponents.DotNetBar.ButtonItem();
+            this.paintingControl1 = new Pilz.UI.PaintingControl();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.bar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.integerInput1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.integerInput2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Transparent;
+            this.panel1.Controls.Add(this.labelX2);
+            this.panel1.Controls.Add(this.labelX1);
+            this.panel1.Controls.Add(this.integerInput2);
+            this.panel1.Controls.Add(this.integerInput1);
+            this.panel1.Controls.Add(this.checkBoxX1);
+            this.panel1.Controls.Add(this.buttonX1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panel1.Location = new System.Drawing.Point(526, 24);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(200, 483);
+            this.panel1.TabIndex = 1;
+            // 
+            // bar1
+            // 
+            this.bar1.AccessibleDescription = "bar1 (bar1)";
+            this.bar1.AccessibleName = "bar1";
+            this.bar1.AccessibleRole = System.Windows.Forms.AccessibleRole.MenuBar;
+            this.bar1.AntiAlias = true;
+            this.bar1.BarType = DevComponents.DotNetBar.eBarType.MenuBar;
+            this.bar1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.bar1.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.bar1.IsMaximized = false;
+            this.bar1.Items.AddRange(new DevComponents.DotNetBar.BaseItem[] {
+            this.buttonItem1,
+            this.buttonItem4,
+            this.buttonItem5});
+            this.bar1.Location = new System.Drawing.Point(0, 0);
+            this.bar1.MenuBar = true;
+            this.bar1.Name = "bar1";
+            this.bar1.Size = new System.Drawing.Size(726, 24);
+            this.bar1.Stretch = true;
+            this.bar1.Style = DevComponents.DotNetBar.eDotNetBarStyle.StyleManagerControlled;
+            this.bar1.TabIndex = 2;
+            this.bar1.TabStop = false;
+            this.bar1.Text = "bar1";
+            // 
+            // buttonItem1
+            // 
+            this.buttonItem1.ButtonStyle = DevComponents.DotNetBar.eButtonStyle.ImageAndText;
+            this.buttonItem1.Image = global::SM64_ROM_Manager.My.Resources.MyIcons.icons8_folder_16px;
+            this.buttonItem1.Name = "buttonItem1";
+            this.buttonItem1.SubItems.AddRange(new DevComponents.DotNetBar.BaseItem[] {
+            this.buttonItem2,
+            this.buttonItem3,
+            this.buttonItem8,
+            this.buttonItem9});
+            this.buttonItem1.Text = "Load profile";
+            // 
+            // buttonItem2
+            // 
+            this.buttonItem2.Name = "buttonItem2";
+            this.buttonItem2.Text = "In-Game";
+            // 
+            // buttonItem3
+            // 
+            this.buttonItem3.Name = "buttonItem3";
+            this.buttonItem3.Text = "Pause Menu";
+            // 
+            // buttonItem4
+            // 
+            this.buttonItem4.ButtonStyle = DevComponents.DotNetBar.eButtonStyle.ImageAndText;
+            this.buttonItem4.Image = global::SM64_ROM_Manager.My.Resources.MyIcons.icons8_save_16px;
+            this.buttonItem4.Name = "buttonItem4";
+            this.buttonItem4.Text = "Save profile";
+            // 
+            // buttonItem5
+            // 
+            this.buttonItem5.ButtonStyle = DevComponents.DotNetBar.eButtonStyle.ImageAndText;
+            this.buttonItem5.Image = global::SM64_ROM_Manager.My.Resources.MyIcons.icons8_settings_16px;
+            this.buttonItem5.Name = "buttonItem5";
+            this.buttonItem5.SubItems.AddRange(new DevComponents.DotNetBar.BaseItem[] {
+            this.buttonItem6,
+            this.buttonItem7,
+            this.buttonItem10});
+            this.buttonItem5.Text = "Options";
+            // 
+            // buttonItem6
+            // 
+            this.buttonItem6.Name = "buttonItem6";
+            this.buttonItem6.Text = "Show Grid";
+            // 
+            // buttonItem7
+            // 
+            this.buttonItem7.Name = "buttonItem7";
+            this.buttonItem7.Text = "Auto snap to Grid";
+            // 
+            // buttonX1
+            // 
+            this.buttonX1.AccessibleRole = System.Windows.Forms.AccessibleRole.PushButton;
+            this.buttonX1.ColorTable = DevComponents.DotNetBar.eButtonColor.OrangeWithBackground;
+            this.buttonX1.Location = new System.Drawing.Point(122, 154);
+            this.buttonX1.Name = "buttonX1";
+            this.buttonX1.Size = new System.Drawing.Size(75, 23);
+            this.buttonX1.Style = DevComponents.DotNetBar.eDotNetBarStyle.StyleManagerControlled;
+            this.buttonX1.TabIndex = 0;
+            this.buttonX1.Text = "Snap to Grid";
+            // 
+            // buttonItem8
+            // 
+            this.buttonItem8.Name = "buttonItem8";
+            this.buttonItem8.Text = "Star Selector";
+            // 
+            // buttonItem9
+            // 
+            this.buttonItem9.Name = "buttonItem9";
+            this.buttonItem9.Text = "File Selection";
+            // 
+            // checkBoxX1
+            // 
+            // 
+            // 
+            // 
+            this.checkBoxX1.BackgroundStyle.CornerType = DevComponents.DotNetBar.eCornerType.Square;
+            this.checkBoxX1.Location = new System.Drawing.Point(3, 98);
+            this.checkBoxX1.Name = "checkBoxX1";
+            this.checkBoxX1.Size = new System.Drawing.Size(100, 23);
+            this.checkBoxX1.Style = DevComponents.DotNetBar.eDotNetBarStyle.StyleManagerControlled;
+            this.checkBoxX1.TabIndex = 1;
+            this.checkBoxX1.Text = "Visible";
+            // 
+            // integerInput1
+            // 
+            // 
+            // 
+            // 
+            this.integerInput1.BackgroundStyle.Class = "DateTimeInputBackground";
+            this.integerInput1.BackgroundStyle.CornerType = DevComponents.DotNetBar.eCornerType.Square;
+            this.integerInput1.ButtonFreeText.Shortcut = DevComponents.DotNetBar.eShortcut.F2;
+            this.integerInput1.Location = new System.Drawing.Point(36, 4);
+            this.integerInput1.Name = "integerInput1";
+            this.integerInput1.ShowUpDown = true;
+            this.integerInput1.Size = new System.Drawing.Size(161, 20);
+            this.integerInput1.TabIndex = 2;
+            // 
+            // integerInput2
+            // 
+            // 
+            // 
+            // 
+            this.integerInput2.BackgroundStyle.Class = "DateTimeInputBackground";
+            this.integerInput2.BackgroundStyle.CornerType = DevComponents.DotNetBar.eCornerType.Square;
+            this.integerInput2.ButtonFreeText.Shortcut = DevComponents.DotNetBar.eShortcut.F2;
+            this.integerInput2.Location = new System.Drawing.Point(36, 33);
+            this.integerInput2.Name = "integerInput2";
+            this.integerInput2.ShowUpDown = true;
+            this.integerInput2.Size = new System.Drawing.Size(161, 20);
+            this.integerInput2.TabIndex = 2;
+            // 
+            // labelX1
+            // 
+            // 
+            // 
+            // 
+            this.labelX1.BackgroundStyle.CornerType = DevComponents.DotNetBar.eCornerType.Square;
+            this.labelX1.Location = new System.Drawing.Point(3, 3);
+            this.labelX1.Name = "labelX1";
+            this.labelX1.Size = new System.Drawing.Size(27, 23);
+            this.labelX1.TabIndex = 3;
+            this.labelX1.Text = "X:";
+            // 
+            // labelX2
+            // 
+            // 
+            // 
+            // 
+            this.labelX2.BackgroundStyle.CornerType = DevComponents.DotNetBar.eCornerType.Square;
+            this.labelX2.Location = new System.Drawing.Point(3, 32);
+            this.labelX2.Name = "labelX2";
+            this.labelX2.Size = new System.Drawing.Size(27, 23);
+            this.labelX2.TabIndex = 3;
+            this.labelX2.Text = "Y:";
+            // 
+            // buttonItem10
+            // 
+            this.buttonItem10.AutoCheckOnClick = true;
+            this.buttonItem10.Name = "buttonItem10";
+            this.buttonItem10.Text = "Simplyfied mode";
+            // 
+            // paintingControl1
+            // 
+            this.paintingControl1.AreaSelectionColor = System.Drawing.Color.CornflowerBlue;
+            this.paintingControl1.AreaSelectionDashStyle = System.Drawing.Drawing2D.DashStyle.DashDot;
+            this.paintingControl1.AutoAreaSelection = true;
+            this.paintingControl1.AutoMoveObjects = true;
+            this.paintingControl1.AutoMultiselection = true;
+            this.paintingControl1.AutoRemoveSelection = true;
+            this.paintingControl1.AutoSingleSelection = true;
+            this.paintingControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.paintingControl1.DrawAreaSelectionMethode = ((Pilz.UI.DelegateDrawPaintingControlAreaSelectionMethode)(resources.GetObject("paintingControl1.DrawAreaSelectionMethode")));
+            this.paintingControl1.DrawGridMethode = ((Pilz.UI.DelegateDrawPaintingControlGridMethode)(resources.GetObject("paintingControl1.DrawGridMethode")));
+            this.paintingControl1.GridChunkSize = new System.Drawing.Size(16, 16);
+            this.paintingControl1.GridColor = System.Drawing.Color.LightGray;
+            this.paintingControl1.GridEnabled = true;
+            this.paintingControl1.GridVisible = true;
+            this.paintingControl1.Location = new System.Drawing.Point(0, 24);
+            this.paintingControl1.Name = "paintingControl1";
+            this.paintingControl1.Offset = ((System.Drawing.PointF)(resources.GetObject("paintingControl1.Offset")));
+            this.paintingControl1.Size = new System.Drawing.Size(526, 483);
+            this.paintingControl1.TabIndex = 0;
+            this.paintingControl1.VisibleForMouseEvents = true;
+            this.paintingControl1.ZoomFactor = new System.Drawing.SizeF(1.5F, 1.5F);
+            // 
+            // HUDEditorForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(726, 507);
+            this.Controls.Add(this.paintingControl1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.bar1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "HUDEditorForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "HUD Editor";
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.bar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.integerInput1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.integerInput2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Pilz.UI.PaintingControl paintingControl1;
+        private System.Windows.Forms.Panel panel1;
+        private DevComponents.DotNetBar.Bar bar1;
+        private DevComponents.DotNetBar.ButtonItem buttonItem1;
+        private DevComponents.DotNetBar.ButtonItem buttonItem2;
+        private DevComponents.DotNetBar.ButtonItem buttonItem3;
+        private DevComponents.DotNetBar.ButtonItem buttonItem4;
+        private DevComponents.DotNetBar.ButtonItem buttonItem5;
+        private DevComponents.DotNetBar.ButtonItem buttonItem6;
+        private DevComponents.DotNetBar.ButtonItem buttonItem7;
+        private DevComponents.DotNetBar.ButtonX buttonX1;
+        private DevComponents.DotNetBar.ButtonItem buttonItem8;
+        private DevComponents.DotNetBar.ButtonItem buttonItem9;
+        private DevComponents.DotNetBar.Controls.CheckBoxX checkBoxX1;
+        private DevComponents.Editors.IntegerInput integerInput2;
+        private DevComponents.Editors.IntegerInput integerInput1;
+        private DevComponents.DotNetBar.LabelX labelX2;
+        private DevComponents.DotNetBar.LabelX labelX1;
+        private DevComponents.DotNetBar.ButtonItem buttonItem10;
+    }
+}
---
+++ SM64 ROM Manager/Rom Manager/GUI/Tools/HUD Editor/HUDEditorForm.cs
@@ -0,0 +1,104 @@
+﻿using DevComponents.DotNetBar;
+using Pilz.UI;
+using SM64Lib.HUD;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SM64_ROM_Manager
+{
+    public partial class HUDEditorForm : OfficeForm
+    {
+
+        // C o n s t r u c t o r
+
+        public HUDEditorForm()
+        {
+            InitializeComponent();
+        }
+
+        // F e a t u r e s
+
+
+
+        // P a i n t i n g O b j e c t s
+
+        private void LoadPaintingObjects(HUDGroup rootGroup)
+        {
+            var objects = new List<PaintingObject>();
+            var curPath = new Stack<string>();
+
+            void checkGroups(IEnumerable<HUDGroup> groups)
+            {
+                foreach (var group in groups)
+                    checkGroup(group);
+            }
+            void checkGroup(HUDGroup group)
+            {
+                curPath.Push(group.Name);
+                checkItems(group.Items);
+                checkGroups(group.Groups);
+                curPath.Pop();
+            }
+            void checkItems(IEnumerable<HUDItem> items)
+            {
+                foreach (var item in items)
+                    checkItem(item);
+            }
+            void checkItem(HUDItem item)
+            {
+                curPath.Push(item.Name);
+                var obj = GetPaintingObject(item);
+                obj.Name = string.Join(".", curPath.ToArray());
+                objects.Add(obj);
+                curPath.Pop();
+            }
+
+            paintingControl1.SuspendDrawing();
+            paintingControl1.PaintingObjects.Clear();
+            paintingControl1.PaintingObjects.AddRange(objects);
+            paintingControl1.ResumeDrawing();
+        }
+
+        private PaintingObject GetPaintingObject(HUDItem item)
+        {
+            var po = new PaintingObject
+            {
+                Tag = item,
+                HardcodedSize = !item.DataInfo.CanSetLocation,
+                Type = PaintingObjectType.Rectangle,
+                EnableFill = false,
+                EnableOutline = true,
+                OutlineColor = Color.Blue,
+                OutlineThicknes = 1,
+                Location = item.Location.Value,
+                Size = new Size(16, 16)
+            };
+
+            // Check for text and set text
+            if (item.DataInfo.CanSetText || !string.IsNullOrEmpty(item.Text))
+            {
+                po.Text = item.Text;
+                po.Type |= PaintingObjectType.Text;
+            }
+
+            // Check for symbol and set image
+            if (false)
+            {
+                // ...
+                po.Type |= PaintingObjectType.Picture;
+            }
+
+            return po;
+        }
+
+        // G U I
+
+    }
+}
